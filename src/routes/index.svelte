82def267211dd6de91bc0b7b74370b03f2688f7b
wolfbyne <flemingrgordon@gmail.com>
2020-09-25 17:14:12 +0200

    Contact form functionality and content adjustments

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -46,6 +46,18 @@
     .card{
         background-color: inherit;
     }
+    #corona{
+        background-color: #6288CF;
+    }
+    #link{
+        color: #004E89;
+    }
+    .icons{
+        color: #212529;
+    }
+    .icons:hover{
+        color: #004E89;
+    }
 </style>
 
 <svelte:head>
@@ -58,6 +70,9 @@
             <h1 class="display-3">A Welcoming Familiy of Worship, Fellowship, and Servanthood</h1>
         </div>
     </div>
+</div>
+<div id="corona" class="pb-3 pr-3 pl-3 mb-2 pt-3 card text-white col text-center">
+    <a href="https://sacoronavirus.co.za" target="_blank" style="color:inherit;"><strong>Stay informed!</strong> Visit the SA Department of Health's website for COVID-19 updates: www.sacoronavirus.co.za</a>
 </div>
 
 <div class="container pt-5 pb-5">
@@ -77,10 +92,22 @@
                 <div class="col-md-4 col-sm-12">
                     <h2>9:30</h2>
                     <h5><strong>We are reopening from 4 Oct.</strong></h5>
-                    <br>
                     <h4 class="pt-3" id="groups">Online Service</h4>
                 </div>
             </div>
+        </div>
+    </div>
+    <h4>Weekly Notices:</h4>
+    <div class="row">
+        <div class="col-md-6 col-sm-6 text-center">
+            <a href="https://drive.google.com/drive/folders/1M4JcYdRS-ZD2_7LHNUqGJ4iRyBL4fxVu?usp=sharing " target="_blank"><i class="fab fa-google-drive fa-4x icons"></i></a>
+            <br><br>
+            <p><strong>Important notices</strong></p>
+        </div>
+        <div class="col-md-6 col-sm-6 text-center">
+            <a href="https://www.youtube.com/channel/UCS3KolbhflbyUfy8BI9F_vQ" target="_blank"><i class="fab fa-youtube fa-4x icons"></i></a>
+            <br><br>
+            <p><strong>Our YouTube channel</strong></p>
         </div>
     </div>
 
@@ -221,8 +248,9 @@
             </div>
         </div>
     </div>
-    <h5>For more information about events happening, visit our <a href="https://www.facebook.com/Sinoville-Methodist-Church-159807964060665/" target="_blank">facebook page.</a></h5>
-    
+    <h5>For more information about activities happening, visit our <a id="link" href="https://www.facebook.com/Sinoville-Methodist-Church-159807964060665/" target="_blank">facebook page.</a></h5>
+    <!-- svelte-ignore a11y-missing-content -->
+    <h5>or contact the Church office: <a id="link" href="mailto:office@sinovillemc.co.za">office@sinovillemc.co.za</a></h5>
     <div class="row">
         <div class="col text-center mb-5 mt-5">
             <h2 class="pb-3">Ministers</h2>
@@ -230,9 +258,7 @@
                 <div class="col-md-4 col-sm-12 pt-5">
                     <h3>Dcn. Douw Grobler</h3>
                     <br>
-                    <a href="tel:012-543-0412"><em>Tel.</em>(012) 543-0412 (H)</a>
-                    <br>
-                    <a href="tel:083-374-4333"><em>Cell.</em>083 374 4333</a>
+                    <a href="tel:083-374-4333"><em>Cell. </em>083 374 4333</a>
                     <br>
                     <a href="mailto:douw@sinovillemc.co.za">douw@sinovillemc.co.za</a>
                 </div>
@@ -242,9 +268,9 @@
                 <div class="col-md-4 col-sm-12 pt-5">
                     <h3>Rev. Storia Seitisho</h3>
                     <br>
-                    <a href="tel:012-567-2465"><em>Tel.</em>(012) 567-2465 (H)</a>
+                    <a href="tel:012-567-2465"><em>Tel. </em>(012) 567-2465 (H)</a>
                     <br>
-                    <a href="tel:072-598-5691"><em>Cell.</em>072 598 5691</a>
+                    <a href="tel:072-598-5691"><em>Cell. </em>072 598 5691</a>
                     <br>
                     <a href="mailto:storia@sinovillemc.co.za">storia@sinovillemc.co.za</a>
                 </div>
@@ -268,7 +294,8 @@
             <h3>Become a volunteer</h3>
             <i class="fas fa-hands-helping fa-7x pt-4 pb-4"></i>
             <p>If you would like to volunteer, you can speak to any of the ministers or society stewards.</p>
-            <p></p>
+            <br>
+            <img src="snap_scan.png" alt="snap_scan">
         </div>
     </div>
 
@@ -285,20 +312,11 @@
                     <p class="pb-5 pt-2"><em>Old Sinoville Methodist Church in Zambezi Drive (now Sefako Makgatho Drive where the Dros is)</em></p>
                 </div>
                 <div class="col-md-6 col-sm-12">
-                    <img class="img-fluid pl-5 pr-5" src="new_sinoville.png" alt="New church">
-                    <p class="pb-5 pt-2"><em>New Sinoville Methodist Church in 3rd Avenue</em></p>
+                    <img class="img-fluid pl-5 pr-5" src="new_sinoville.png" alt="old church">
+                    <p class="pb-5 pt-2"><em>Old Sinoville Methodist Church in 3rd Avenue</em></p>
                 </div>
             </div>
             <p>Later they started a Methodist Society which met in the Northridge Primary School Hall.  Eventually enough funds were raised to build a small Church.</p>
-            <div class="row pt-4 pb-4">
-                <div class="col-md-6 col-sm-12">
-                    <img class="img-fluid pl-5 pr-5" src="building_church.png" alt="old church">
-                </div>
-                <div class="col-md-6 col-sm-12">
-                    <img class="img-fluid pl-5 pr-5" src="building_church2.png" alt="New church">
-                </div>
-            </div>
-            <p >In 1982 the original Sinoville Methodist Church on Zambezi Drive (where Montana Crossing is today) was opened.  Then in 1996 the original church was enlarged.  Soon after the renovations, the building was sold as part of a turnkey deal and they relocated to the current property on 25 July 1999.  The new property included a hall and Sunday School buildings (which the original church did not have) as well as a property of 2 hectares..</p>
             <div class="row pt-4 pb-4">
                 <div class="col-md-6 col-sm-12">
                     <img class="img-fluid pl-5 pr-5" src="bishop.png" alt="old church">
@@ -308,12 +326,25 @@
                     <img class="img-fluid pl-5 pr-5" src="gathering.png" alt="New church">
                 </div>
             </div>
-
+            <p >In 1982 the original Sinoville Methodist Church on Zambezi Drive (where Montana Crossing is today) was opened.  Then in 1996 the original church was enlarged.  Soon after the renovations, the building was sold as part of a turnkey deal and they relocated to the current property on 25 July 1999.  The new property included a hall and Sunday School buildings (which the original church did not have) as well as a property of 2 hectares..</p>
+            <div class="row pt-4 pb-4">
+                <div class="col-md-6 col-sm-12">
+                    <img class="img-fluid pl-5 pr-5" src="building_church.png" alt="old church">
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <img class="img-fluid pl-5 pr-5" src="building_church2.png" alt="New church">
+                </div>
+                <div class="col-12 mt-5">
+                    <img class="img-fluid pl-5 pr-5" src="current_church.jpg" alt="Current church">
+                </div>
+            </div>
+            <em>Thank you for the photos to Monty and Lettie Morris</em>
+            <br><br>
             <h4 class="about">Our Vision</h4>
-            <p>A Welcoming Family of Worship, Fellowship and Servanthood - the practical outflowing of who we are as Jesus’ Followers. Making Disciples - obedience to Jesus’ call in Matt 28:18</p>
-            <p>Believing that the Church is called to make Disciples who are committed to the Mission of God as demonstrated in Jesus words and teaching; SMC strives to be a place where people can meet with the Risen Christ, Engage with His teachings, Heed His call to follow and, filled by the His Spirit, live each day for God’s Kingdom.</p>
+            <p>A welcoming family of worship, fellowship and servanthood – the practical outflowing of who we are as Jesus’ followers.</p>
+            <p>We believe that the Church is called to make disciples who are committed to the mission of God as demonstrated in Jesus words and teaching. SMC strives to be a place where people can meet with the Risen Christ, engage with His teachings, heed His call to follow and be filled with His Holy Spirit.</p>
             <h4 class="pt-4 about">Our Community</h4>
-            <p id="contact">We are a vibrant mix of cultures, backgrounds and people who worship predominantly in the common language of English.  As a result our Leadership Team reflects who we are. </p>
+            <p id="contact">We are a vibrant mix of cultures, backgrounds and people who worship predominantly in the common language of English. There is a place for everyone.</p>
         </div>
     </div>
 
@@ -332,7 +363,7 @@
             <a href="mailto:office@sinovillemc.co.za"><em>Email:</em> office@sinovillemc.co.za</a>
         </div>
         <div class="col-md-6 col-sm-12 pt-5">
-            <form name='contact' method='POST'><input type='hidden' name='form-name' value='contact' />
+            <form name='contact' method='POST' data-netlify="true">
                 <fieldset>
                 <div class="form-group">
                     <!-- svelte-ignore a11y-label-has-associated-control -->
@@ -353,7 +384,7 @@
                 <div class="form-group">
                     <!-- svelte-ignore a11y-label-has-associated-control -->
                     <label>Subject</label>
-                    <input type="email" class="form-control" aria-describedby="emailHelp" placeholder="Enter your subject" name="subject">
+                    <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="Enter your subject" name="subject">
                 </div>
                 <div class="form-group">
                     <!-- svelte-ignore a11y-label-has-associated-control -->
