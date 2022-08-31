62a6a4e7755c2641aaf4dc605d3c6cb346ab20c1
Gordon Fleming <flemingrgordon@gmail.com>
2022-08-31 23:59:04 +0200

    plain html css and javascript conversion

--- README.md
+++ README.md
@@ -1,12 +1,3 @@
 ### Netlify
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/c88849fc-9a22-48a4-a7c8-8119cdc8a273/deploy-status)](https://app.netlify.com/sites/stoic-davinci-83351f/deploys)
-
-**Build command**
-```
-npm run export
-```
-**Publish directory**
-```
-__sapper__/export
-```
---
+++ dist/script.js
@@ -0,0 +1,12 @@
+function scrollDom1(event) {
+    document.getElementById('about').scrollIntoView(true);
+}
+function scrollDom2(event) {
+    document.getElementById('groups').scrollIntoView(true);
+}
+function scrollDom3(event) {
+    document.getElementById('giving').scrollIntoView(true);
+}
+function scrollDom4(event) {
+    document.getElementById('contact').scrollIntoView(true);
+}
